e5029064d97f432b12eff53f7ac687f5ac6f8e09
anuptamang <anupkumartamang@gmail.com>
2019-06-30 16:18:02 +0545

    added hot reload, babel polyfill,fixed devServer path and removed build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src'),
+    entry: {
+        app: [
+            '@babel/polyfill',
+            './src/app.js',
+        ]
+    },
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        port: 3000,
-        contentBase: path.resolve(__dirname, 'build')
+        filename: 'app.bundle.js'
     },
 
     module: {
