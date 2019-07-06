d3c7805655cc0e5242191223bcd026857889c644
anuptamang <anupkumartamang@gmail.com>
2019-07-06 17:20:04 +0545

    project folder str. updated

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
         ]
     },
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'src/build'),
         filename: 'app.bundle.js'
     },
 
